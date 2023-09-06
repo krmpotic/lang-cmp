c93c23dd089209ae9482c990919ea258a2543250
Luka Krmpotic <luka.krmpotic@gmail.com>
2023-09-06 22:43:32 +0200

    print args, normal order, no space

---
+++ revargs/revargs.asm
@@ -0,0 +1,51 @@
+section .data
+	SYS_WRITE equ 1
+	SYS_EXIT equ 60
+	STDOUT equ 1
+	NL db 0xa
+	ASCII_SPACE db 0x20
+
+section .text
+	global _start
+_start:
+	pop r15     ;argc
+	mov rbx, rsp;**argv
+  loopx:
+	dec r15
+	cmp r15, 0
+	je exit     ;no more args left
+
+	add rbx, 8
+	jmp puts
+
+	jmp loopx
+
+puts:
+	mov r12, 0
+c:
+	mov rsi, [rbx]
+	add rsi, r12
+
+	mov cl, [rsi]
+	cmp cl, 0
+	je loopx
+
+	mov rax, SYS_WRITE
+	mov rdi, 1
+	mov rdx, 1 
+	syscall
+
+	inc r12
+	jmp c
+space:
+	mov rsi, [ASCII_SPACE]
+	mov rax, SYS_WRITE
+	mov rdi, 1
+	mov rdx, 1 
+	syscall
+	je loopx
+
+exit:
+	mov rax, SYS_EXIT
+	mov rdi, 0
+	syscall
